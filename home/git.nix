50823a164768f624cb638530a7f2cc1cb6b705dc
lzabry <lzabry@nixos.localdomain>
2026-04-29 14:37:18 +0900

    Modified git config

--- home/git.nix
+++ home/git.nix
@@ -1,6 +1,10 @@
 {
   programs.git = {
     enable = true;
+
+    userName = "wangzhengji06";
+    userEmail = "wangzhengji06@gmail.com";
+
     settings = {
       init.defaultBranch = "main";
       pull.rebase = true;
